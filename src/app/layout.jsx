faa1a3188b412cd0a5f912e9bb0f8ff35de60a74
AhmedOg <ahmedogbinto@gmail.com>
2025-08-25 16:40:05 -0400

    Creation des layouts et de la page d'accueil.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,19 @@
 import "./globals.css"
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full ">
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <body>{children}</body>
+      <body className="min-h-full flex flex-col">
+         <Navbar />
+          <main className="grow">{children}</main>
+         <Footer />
+      </body>
+     
     </html>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <nav className="fixed w-full bg-slate-50 border-b border-b-zinc-300">
+      <div className="u-main-container flex py-4 px-12">
+        <Link href="/" className="mr-2 text-zinc-900">
+          AXORIA
+        </Link>
+        <Link href="/categories" className="mx-2 text-zinc-900 mr-auto">
+          Cartegories
+        </Link>
+        <Link href="/categories/create" className="mx-2 text-zinc-900">
+          Add an article
+        </Link>
+      </div>
+    </nav>
+  );
+}
