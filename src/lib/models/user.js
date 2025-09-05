ae86a94a061caeb1f9299186f135f522fcda365f
AhmedOg <ahmedogbinto@gmail.com>
2025-09-05 18:31:50 -0400

    Creation UX de la page d 'inscription et creation du model user

---
+++ src/lib/models/user.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nomalizedUserName: {                       // c'est comme un type de slug pour les noms d'utilisateur.
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: Dtring,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+    // isAdmin: {
+    //     type: Boolean,
+    //     default: false
+    // }
+})
+
+export const User = mongoose.models?.User || mongoose.model("User", userSchema)
