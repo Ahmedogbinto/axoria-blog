a66b9c0decfc5c7bbc26222e7aaf923788dce847
AhmedOg <ahmedogbinto@gmail.com>
2025-09-23 00:23:43 -0400

    Creation et mise en place du compte BunnyCDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com'], // Ajoutez le domaine de Firebase ici
+   // domains: ['firebasestorage.googleapis.com'], // Ajoutez le domaine de Firebase ici
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ReplicationAxoria.b-cdn.net",
+        pathname: "/**"
+      }
+    ]
+
   },
 };
 
